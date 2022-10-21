752c8c3a5d87f1c6db24d2d2a647b89c0be206dc
carlosriveraf <carlos_2017_1@hotmail.com>
2022-10-21 16:22:15 -0500

    creación de fichero mediante una subida, tamaño máximo 500kb

---
+++ app/Http/Controllers/Api/V1/FicheroController.php
@@ -0,0 +1,82 @@
+<?php
+
+namespace App\Http\Controllers\Api\V1;
+
+use App\Http\Controllers\Controller;
+use App\Models\Api\V1\Fichero;
+use Illuminate\Http\Request;
+
+class FicheroController extends Controller
+{
+    public function listarFicheros()
+    {
+        $rsFicheros = Fichero::all();
+
+        $listadoFicheros = [];
+        foreach ($rsFicheros as $fichero) {
+            $listadoFicheros[] = [
+                'nombre' => $fichero->nombre,
+                'extension' => $fichero->extension,
+            ];
+        }
+
+        echo '<pre>';
+        var_dump($listadoFicheros);
+        echo '</pre>';
+    }
+
+    public function guardarFichero(Request $request)
+    {
+        if (!isset($request->archivo)) {
+            return response()->json([
+                'code' => 400,
+                'status' => 'fail',
+                'message' => 'Debe subir el archivo.',
+            ]);
+        }
+
+        if (!$request->archivo->isValid()) {
+            return response()->json([
+                'code' => 400,
+                'status' => 'fail',
+                'message' => 'Error inesperado.',
+            ]);
+        }
+
+        if (filesize($request->archivo) > 500000) {
+            return response()->json([
+                'code' => 400,
+                'status' => 'fail',
+                'message' => 'El archivo no debe pesar más de 500kb.',
+            ]);
+        }
+
+        $extension = $request->archivo->getClientOriginalExtension();
+
+        if ($extension == '') {
+            return response('El archivo no tiene extensión.', 400);
+        }
+
+        $nombre = substr($request->archivo->getClientOriginalName(), 0, -1 * (strlen($extension) + 1));
+
+        $fichero = new Fichero();
+        $fichero->nombre = $nombre;
+        $fichero->extension = $extension;
+        $fichero->contenido = $request->archivo->get();
+        $fichero->save();
+
+        if ($fichero->save()) {
+            return response()->json([
+                'code' => 201,
+                'status' => 'success',
+                'message' => 'Archivo subido con éxito.',
+            ]);
+        }
+
+        return response()->json([
+            'code' => 500,
+            'status' => 'fail',
+            'message' => 'Hubo problemas al subir el archivo.',
+        ]);
+    }
+}
---
+++ app/Models/Api/V1/Fichero.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Models\Api\V1;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Fichero extends Model
+{
+    use HasFactory;
+
+    /**
+     * The table associated with the model.
+     *
+     * @var string
+     */
+    protected $table = 'ficheros';
+}
