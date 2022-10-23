4cb435824ddc0e45a5d2a68525b22a54c74e8f03
carlosriveraf <carlos_2017_1@hotmail.com>
2022-10-23 02:09:02 -0500

    creación de middleware para el api | token necesario en el header

---
+++ app/Http/Middleware/AuthToken.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+
+class AuthToken
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure(\Illuminate\Http\Request): (\Illuminate\Http\Response|\Illuminate\Http\RedirectResponse)  $next
+     * @return \Illuminate\Http\Response|\Illuminate\Http\RedirectResponse
+     */
+    public function handle(Request $request, Closure $next)
+    {
+        if ($request->header('token') !== '12345') {
+            return response()->json([
+                'code' => 401,
+                'status' => 'fail',
+                'message' => 'Token no encontrado o no coincide.',
+            ]);
+        }
+
+        return $next($request);
+    }
+}
